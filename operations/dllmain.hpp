bd6f5a91246592c0aaf23dfdd75943f29080e2b5
Mouradouchane <mouradouchane@gmail.com>
2023-03-27 13:05:32 +0000

    add:few function as testing parsing

--- operations/dllmain.hpp
+++ operations/dllmain.hpp
@@ -1,15 +1,12 @@
 
 #pragma once
 
-#define TYPE LPWSTR
+#define TYPE char
 
 // extern "C" __declspec(dllexport) TYPE parse_expression( TYPE );
 
 extern "C" __declspec(dllexport) void parse_expression( TYPE& str_exp ) {
 
-	str_exp[0] = L'M';
-	str_exp[1] = L'M';
-	str_exp[2] = L'M';
 
 	// return expression_string_lenght;
 
