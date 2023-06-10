b43656be1c0880006d4793ab601eec3cac49a52b
Mouradouchane <mouradouchane@gmail.com>
2023-06-10 21:35:19 +0000

    still working on parser functions

--- operations/parser.cpp
+++ operations/parser.cpp
@@ -24,28 +24,6 @@ static short operator_level(char const& ch) {
 
 }
 
-static bool is_operator( char const& ch ) {
-
-	switch (ch) {
-
-		case '+': case '-': {
-			return true;
-		}
-
-		case '/': case '%': case '*': case '^' : {
-			return true;
-		} 
-
-		case '(': case ')': {
-			return true;
-		}
-
-	}
-
-	return false;
-}
-
-
 static bool is_valid_expression(std::string const& math_expression) {
 
 	int32_t balance = 0;
@@ -69,13 +47,62 @@ static bool is_valid_expression(std::string const& math_expression) {
 
 }
 
+static bool is_operator( char const& ch ) {
+
+	switch (ch) {
+
+		case '+': case '-': {
+			return true;
+		}
+
+		case '/': case '%': case '*': case '^' : {
+			return true;
+		} 
+
+		case '(': case ')': {
+			return true;
+		}
+
+	}
+
+	return false;
+}
+
+static void get_sub_expression_end( std::string & expression , size_t & index ) {
+
+	INT32 balance = 1;
+
+	index += 1;
+	for (		; index < expression.size() ; index += 1 ) {
+
+		if (expression[index] == '(') balance++ ;
+
+		if (expression[index] == ')') {
+			balance-- ;
+
+			// balance == 0 with ')' mean we find the end of this sub-expression
+			if ( balance == 0 ) break; 
+		}
+
+	}
+
+}
+
 static void parse_expression( node& expression_node ) {
 	
+	// to keep track the last low operator precedence
 	short  last_op_level = 6;
 	size_t last_op_index = 0;
 
+	// search for last low operator to parse depend on it
 	short current_op_level = 6;
 	for (size_t i = 0; i < expression_node.value.size(); i++ ) {
+
+		if ( expression_node.value[i] == '(' ) {
+
+			get_sub_expression_end(expression_node.value , i);
+
+		}
 
 		current_op_level = operator_level(expression_node.value[i]);
 
@@ -84,7 +111,17 @@ static void parse_expression( node& expression_node ) {
 			last_op_index = i;
 		}
 
+
 	}
+
+	// parse the expression based on the last lowest operator found
+
+
+	// mark this node with 
+
+
+	// go to left and right nodes
+
 
 }
 
