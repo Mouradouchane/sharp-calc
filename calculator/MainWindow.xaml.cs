b08c06a66dcb61b6138206980e489ba9523d8297
Mouradouchane <mouradouchane@gmail.com>
2023-10-14 05:08:21 +0100

    a nice progress on calculator gui

--- calculator/MainWindow.xaml.cs
+++ calculator/MainWindow.xaml.cs
@@ -15,16 +15,165 @@ using System.Windows.Shapes;
 
 namespace calculator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+
+        public class TextBox : Control
+        {
+            
+        }
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-  
+        public void AddNumberToExpression( int value )
+        {
+            MessageBox.Show($"Button {value} is clicked!");
+        }
+
+        public void AddCharToExpression(char value)
+        {
+            MessageBox.Show($"Button '{value}' is clicked!");
+        }
+
+        public void AddOperatorToExpression(char value)
+        {
+            MessageBox.Show($"Button '{value}' is clicked!");
+        }
+
+        public void AddValueToExpression( double value )
+        {
+            MessageBox.Show($"Button value={value} is clicked!");
+
+        }
+
+        private void N0_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddNumberToExpression(0);
+        }
+
+        private void N1_Button_OnClick(object sender, RoutedEventArgs e){
+            AddNumberToExpression(1);
+        }
+
+        private void N2_Button_OnClick(object sender, RoutedEventArgs e ) { 
+            AddNumberToExpression(2); 
+        }
+
+        private void N3_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddNumberToExpression(3);
+        }
+
+        private void N4_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddNumberToExpression(4);
+        }
+
+        private void N5_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddNumberToExpression(5);
+        }
+
+        private void N6_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddNumberToExpression(6);
+        }
+
+        private void N7_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddNumberToExpression(7);
+        }
+
+        private void N8_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddNumberToExpression(8);
+        }
+
+        private void N9_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddNumberToExpression(9);
+        }
+
+        private void FloatPoint_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddCharToExpression('.');
+        }
+
+
+        private void BracketOpen_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddCharToExpression('(');
+        }
+
+        private void BracketClose_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddCharToExpression(')');
+        }
+        
+        private void FunctionBracket_OpenButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddCharToExpression('{');
+        }
+
+        private void FunctionBracket_CloseButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddCharToExpression('}');
+        }
+
+        
+        private void OperatorPlus_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddOperatorToExpression('+');
+        }
+
+        private void OperatorMinus_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddOperatorToExpression('-');
+        }
+
+        private void OperatorMultiplay_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddOperatorToExpression('*');
+        }
+
+        private void OperatorMod_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddOperatorToExpression('%');
+        }
+
+        private void OperatorDivision_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddOperatorToExpression('/');
+        }
+
+        private void OperatorPower_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddOperatorToExpression('^');
+        }
+
+        private void Pi_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+            AddValueToExpression( Math.PI );
+        }
+
+        private void Euler_Button_OnClick(object sender , RoutedEventArgs e)
+        {
+            AddValueToExpression(Math.E);
+        }
+
+        private void Clear_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClearAll_Button_OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
     }
 }
