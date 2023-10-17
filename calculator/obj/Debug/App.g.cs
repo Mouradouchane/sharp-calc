7b776abc179ad2556b37e3afdeccf8935d87311c
Mouradouchane <mouradouchane@gmail.com>
2023-10-17 02:59:14 +0100

    add icon + few edits on the gui elemetns
    todo:work on history list

--- calculator/obj/Debug/App.g.cs
+++ calculator/obj/Debug/App.g.cs
@@ -58,7 +58,7 @@ namespace calculator {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/calculator;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/HeavyCalc;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
